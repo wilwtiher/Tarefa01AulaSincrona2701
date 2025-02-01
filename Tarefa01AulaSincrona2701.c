de71745cfcd16143f6c9b5e85883e5137ecdbe28
Wilton Júnior <wiltonjrtablet@gmail.com>
2025-02-01 12:44:34 -0300

    config inicial dos pinos da interrupcao

--- Tarefa01AulaSincrona2701.c
+++ Tarefa01AulaSincrona2701.c
@@ -2,13 +2,32 @@
 #include "pico/stdlib.h"
 
 
+// Configurações dos pinos 
+const uint led_pin = 13;    //Red=13, Blue=12, Green=11
+const uint botao_pinA = 5;        // Botão A = 5, Botão B = 6 , BotãoJoy = 22
+const uint botao_pinB = 6;        // Botão A = 5, Botão B = 6 , BotãoJoy = 22
+// Prototipo da função de interrupção
+static void gpio_irq_handler(uint gpio, uint32_t events);
 
 int main()
 {
-    stdio_init_all();
+    gpio_init(led_pin);                 // Inicializa o pino do LED
+    gpio_set_dir(led_pin, GPIO_OUT);    // Configura o pino como saída
+    gpio_init(botao_pinA);                    // Inicializa o botão
+    gpio_set_dir(botao_pinA, GPIO_IN);        // Configura o pino como entrada
+    gpio_pull_up(botao_pinA);                 // Habilita o pull-up interno
+    gpio_init(botao_pinB);                    // Inicializa o botão
+    gpio_set_dir(botao_pinB, GPIO_IN);        // Configura o pino como entrada
+    gpio_pull_up(botao_pinB);                 // Habilita o pull-up interno
+    // Configuração da interrupção com callback
+    gpio_set_irq_enabled_with_callback(botao_pinA, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(botao_pinB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
     }
+}
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    bool estado_atual = gpio_get(led_pin); // Obtém o estado atual
+    gpio_put(led_pin, !estado_atual);      // Alterna o estado
 }
